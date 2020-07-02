27076b1f5ee297bc8aa5dd5072485bdfe94e5987
Elias Khan <38148675+eliashkan@users.noreply.github.com>
2020-07-02 21:38:13 +0200

    Update IceCreamSeller.java

--- IceCreamSeller.java
+++ IceCreamSeller.java
@@ -3,4 +3,11 @@ package be.intecbrussel.sellers;
 import be.intecbrussel.eatables.*;
 
 public interface IceCreamSeller extends Profitable {
+    // interface with abstract order methods for each icecream type
+    // with throw exception specification necessary because implementation
+    // of these methods throw this exception.
+    Cone orderCone(Cone.Flavor[] balls) throws NoMoreIceCreamException;
+    IceRocket orderIceRocket() throws NoMoreIceCreamException;
+    Magnum orderMagnum(Magnum.MagnumType type) throws NoMoreIceCreamException;
+
 }
